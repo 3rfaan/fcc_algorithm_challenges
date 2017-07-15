8ebd89f7e4d1e12adb776413df24545f2d0f79a3
3rfaan <zubi.arfan@gmail.com>
2017-07-15 22:40:27 +0200

    Update Find the Longest Word in a String.js

--- Find the Longest Word in a String.js
+++ Find the Longest Word in a String.js
@@ -1,13 +1,13 @@
 function findLongestWord(str) {
     var words = str.split(" ");
-    var limit = 0;
+    var longestWord = 0;
 
     for (var i = 0; i < words.length; i++) {
-        if (words[i].length > limit) {
-            limit = words[i].length;
+        if (words[i].length > longestWord) {
+            longestWord = words[i].length;
         }
     }
-    return limit;
+    return longestWord;
 }
 
 findLongestWord("The quick brown fox jumped over the lazy dog");
