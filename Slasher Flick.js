ee560f59949d6ab7cf21e6174f6ede3947fa7ec4
3rfaan <zubi.arfan@gmail.com>
2017-08-03 15:14:08 +0200

    Add files via upload

---
+++ Slasher Flick.js
@@ -0,0 +1,11 @@
+function slasher(arr, howMany) {
+    return arr.slice(howMany);
+}
+
+slasher([1, 2, 3], 2);
+
+// SPLICE METHOD
+/* function slasher(arr, howMany) {
+     arr.splice(0, howMany);
+     return arr;
+} */
